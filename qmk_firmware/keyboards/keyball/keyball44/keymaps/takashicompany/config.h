32ad32679284802540b29ea0b40dcaf882cef4de
musashitak <123928839+musashitak634@users.noreply.github.com>
2024-05-18 09:47:10 +0900

    Update config.h
    #define TAPPING_TERM 170
    #define HOLD_ON_OTHER_KEY_PRESS

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/takashicompany/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/takashicompany/config.h
@@ -35,6 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
-#define TAPPING_TERM 140
+#define TAPPING_TERM 170
 #define HOLD_ON_OTHER_KEY_PRESS
 
