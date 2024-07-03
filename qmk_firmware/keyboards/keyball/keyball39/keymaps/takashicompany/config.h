2ef64eac11e78b695d211ff007d772d48f8afdaa
musashitak <123928839+musashitak634@users.noreply.github.com>
2024-07-03 20:56:59 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/takashicompany/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/takashicompany/config.h
@@ -35,7 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
-#define TAPPING_TERM_PER_KEY
 #define PERMISSIVE_HOLD
 #define PREVENT_STUCK_MODIFIERS
 #define IGNORE_MOD_TAP_INTERRUPT
